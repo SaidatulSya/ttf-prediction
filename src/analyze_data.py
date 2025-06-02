7e88d764bd167eb857cf353e70f54eb6eeb0c710
SaidatulSya <saidatul.syafiqah95@gmail.com>
2025-06-02 19:46:47 +0800

    Update analyze_data.py

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class AnalyzeData:
@@ -9,7 +10,7 @@ class AnalyzeData:
     df.shape() -- to view the row x column of the dataset
     df.info() -- to view total number of non-null 
     """
-    def __init__(self, df: pd.DataFrame, interval, filename=None):
+    def __init__(self, df: pd.DataFrame, filename=None):
         if df is None or df.empty:
             raise ValueError("Provided DataFrame is empty or None.")
 
@@ -19,12 +20,6 @@ class AnalyzeData:
                 df = df.set_index("Timestamp")
             else:
                 raise ValueError("Timestamp column not found. Please ensure the column is named 'Timestamp'")
-                
-        if interval.endswith("m"):
-            self.interval = interval.replace("m", "min")
-        else:
-            self.interval = interval
-        
                     
         self.df = df
         self.filename = filename or "output"
@@ -86,6 +81,33 @@ class AnalyzeData:
         print("Imputation complete")
         return self.cleaned_df
 
+    def outliers_bounds(self,value_col = "value"):
+        q1 = np.percentile(self.df[value_col], 25)
+        q3 = np.percentile(self.df[value_col], 75)
+        iqr = q3-q1
+
+        lb = q1 - 1.5 * iqr
+        ub = q3 + 1.5 * iqr
+
+        print('Lower bound:', round(lb,2))
+        print('Upper bound:', round(ub,2))
+        return lb, ub
+
+    def remove_outliers(self, value_col="value"):
+        lb, ub = self.outliers_bounds(value_col)
+        original_len = len(self.df)
+        self.df = self.df[(self.df[value_col] >= lb) & (self.df[value_col] <= ub)]
+        print(f"Removed {original_len - len(self.df)} outliers.")
+        
+
+    def box_plot(self, value_col="value"):
+        plt.figure(figsize=(10,2))
+        self.df.boxplot(column=value_col, vert=False, grid=True)
+        plt.title(f"Boxplot of {value_col}")
+        plt.grid(True)
+        plt.show()
+        
+
     def save_to_csv(self):
         if self.cleaned_df is not None:
             filename = f"{self.filename}_cleaned.csv"
@@ -93,4 +115,5 @@ class AnalyzeData:
             print(f"Saved cleaned data to: {filename}")
         else:
             print("No cleaned data available to save.")
-            
+
+        
